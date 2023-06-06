9327bc783812c52c8946b1d19d06f1a7ca0caea0
Xavier <xaviramos668@gmail.com>
2023-06-06 00:32:51 -0300

    Finalizado

---
+++ js/bg.js
@@ -0,0 +1,13 @@
+const bg = document.querySelector(".inicio_logo");
+let index = 1;
+
+function prueba() {
+  if (index >= 4) {
+    index = 1;
+  }
+  bg.style.backgroundImage = 'url("img/bg' + index + '.jpg")';
+  index++;
+}
+
+// Solo hay tres imágenes, así que el intervalo debería ser de 8000 ms (8 segundos)
+setInterval(prueba, 5000);
